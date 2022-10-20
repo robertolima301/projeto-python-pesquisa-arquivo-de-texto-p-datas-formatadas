8546a3acd94e1cbdf22e7adf41284dece303978d
robertolima301 <59844858+robertolima301@users.noreply.github.com>
2022-10-20 16:57:36 -0300

    Update datas-formatadas.py

--- datas-formatadas.py
+++ datas-formatadas.py
@@ -28,34 +28,3 @@ file.close()
 newfile.close()
 
 
-##################################################### novo código ###################################################                   
-#Simulador de dado de 6faces
-#Simular de um dado de 6faces, gerando valor de 1 até 6
-
-import random
-
-class Simulardado:
-     def __init__(self):
-        self.valor_minino = 1
-        self.valor_maximo = 6
-        self.frase = " Gostaria de gerar um valor para o dado? "
-
-     def Iniciar(self):
-        resposta = input(self.frase)
-        try:
-            if resposta == "sim" or resposta == "s":
-                self.Valordodado()
-            elif resposta == "não" or resposta == "n":
-                print("Agradecemos a sua participação!")
-            else:
-                print("Digite sim ou não")
-           
-        except:
-            print("Ocorreu um erro ao receber sua resposta")
-
-    
-     def Valordodado(self):
-        print(random.randint(self.valor_minino,self.valor_maximo))
-
-simulador = Simulardado()
-simulador.Iniciar()
